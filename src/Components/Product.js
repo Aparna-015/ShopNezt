c841fa9690a549017135abc9b8d7034f5ed7b002
Aparna <aparnaraman995@gmail.com>
2025-02-03 11:35:05 -1200

    Updated

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Product = ({products}) => {
-    const { id,title, price,image} = products;
+const Product = ({ products }) => {
+  const { id, title, price, image } = products;
   console.log(title);
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
-  const handleclick=(id)=>{
-console.log(id,"id");
+  const handleclick = (id) => {
+    console.log(id, "id");
 
-navigate(`/product/${id}`)
-
-  }  
+    navigate(`/product/${id}`);
+  };
   return (
-<div className='product'>
-    
-<div className='product_section'>
-
-
-<img src={image} alt=""/>
-<h1>{title}</h1>
-<h1>{price}</h1>
-    <div>
-<button className='button_section' onClick={()=>handleclick(id)}>ViEW MORE</button>
-</div>
+    <div className="product">
+      <div className="product_section">
+        <img src={image} alt="" />
+        <h1>{title}</h1>
+        <h1>{price}</h1>
+        <div>
+          <button className="button_section" onClick={() => handleclick(id)}>
+            View more
+          </button>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
